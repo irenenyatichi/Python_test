171009ba3f24ba985047f6bcb5f91908d4e3cf76
irenenyatichi <irenenyatichik@gmail.com>
2021-10-06 11:14:11 +0300

    Adding Comments

--- LogIn/models.py
+++ LogIn/models.py
@@ -4,21 +4,23 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
 class User(models.Model):
+# The fields required while logging in.
     email=models.EmailField(max_length=30, default='Email')
     password = models.CharField(max_length=15, default='Password')
 
 class Project(models.Model):
+# The fields required while creating a new project.
     project_name = models.TextField(max_length=50 , default= "Project Name")
     project_description = models.CharField(max_length=500 , default="Project Description")
 
 class Simulation(models.Model):
+# The fields required while simulating the new project.
     simulation_name = models.CharField(max_length=30, default="Simulation Name")
     date = models.DateField()
 
 class Details(models.Model):
+# These are the relationships between the models.
     user = models.ManyToOneRel(User,to=Project and Simulation, on_delete=models.CASCADE, field_name = None)
     project = models.OneToOneRel(Project, to=Simulation, on_delete=models.CASCADE, field_name = None)
     simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
--- LogIn/tests.py
+++ LogIn/tests.py
@@ -3,40 +3,44 @@ from django.forms.widgets import EmailInput
 from django.test import TestCase
 from .models import User,  Project,  Simulation
 
-# Create your tests here.
+# The Tests
 
 class UserTestCase(TestCase):
+# Details based on an account created.
     def setUp(self):
         self.user = User.objects.create(
             email = "irenenyatichik@gmail.com",
             password = "dghkj100",
         )
         self.project = Project.objects.create(
-            # user=User(email = "irenenyatichik@gmail.com",password = "dghkj100"),
             project_name = "Irene Nyatichi",
             project_description = "This project is based on the assessment given.",
         )
         self.simulation = Simulation.objects.create(
-            # project=Project(project_name = "Irene Nyatichi",project_description = "This project is based on the assessment given."),
             simulation_name = "First Simulation",
             date = "2021-10-05",
         )
 
 
     def test_login_credentials(self):
+    #To ensure that the user is logged in.
         self.assertEqual(self.user.email,"irenenyatichik@gmail.com" )
         self.assertEqual(self.user.password, "dghkj100")
 
 
     def test_create_project(self):
-        user = self.user
-        self.assertTrue(isinstance(self.user , User))
+    #To ensure that the user is logged in before creating a project.
+        user = self.user     
+        self.assertTrue(isinstance(self.user , User))   
+    #This is the code for testing the creation of the project
         self.assertEqual(self.project.project_name,"Irene Nyatichi")
         self.assertEqual(self.project.project_description,"This project is based on the assessment given.")
 
 
     def test_simulation(self):
-        project = self.project
+    #To ensure that the logged in user has a project before simulating it.
+        project = self.project  
         self.assertTrue(isinstance(self.project and self.user , User))
+    #This is the code for testing the simulation of the project
         self.assertEqual(self.simulation.simulation_name, "First Simulation")
         self.assertEqual(self.simulation.date, "2021-10-05")
